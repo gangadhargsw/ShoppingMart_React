7276b9bf6aa5aa0c55707bc21d864712e59f14b9
gangadhar swain <gangadharswain6@gmail.com>
2023-03-10 17:04:48 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Component } from 'react';
+import Abc from './abc';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to New App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Abc></Abc>
+      </div>
+    );  
+  }
 }
 
 export default App;
